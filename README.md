02fac9e4a99a0d0b5c592c5cd1a4bb4caa15a58f
James Moulds <jimmymoulds@gmail.com>
2020-09-24 14:10:01 +0100

    initial app commit with requirements

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Iris-Heroku
 Simple Iris App deployed on Heroku
+
+#Required Files
+1 setup.sh
+2 Procfile
+3 requirements.txt
---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    """
+    A Simple Iris EDA App
+    """
+
+    st.title("Iris EDA APP with streamlit")
+    st.subheader("Streamlit is Cool")
+
+if __name__ == '__main__':
+    main()
